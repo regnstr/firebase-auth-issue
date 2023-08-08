cd6fb69368368d270432eba03fd24b040ab508b3
Isak Regnström <isak.regnstrom@gmail.com>
2023-08-08 10:55:46 +0200

    final commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,54 @@
 <script lang="ts">
-    import Auth from "$lib/components/Auth.svelte";
+    import { signOut, getAuth, signInWithRedirect, GoogleAuthProvider, onAuthStateChanged, type User } from "firebase/auth";
+    import { initializeApp, getApp } from "firebase/app";
+    import { getFirestore } from "firebase/firestore";
+
+    const firebaseConfig = {
+        // PASTE FIREBASE CONFIG HERE
+        // PASTE FIREBASE CONFIG HERE
+        // PASTE FIREBASE CONFIG HERE
+        // PASTE FIREBASE CONFIG HERE
+        // PASTE FIREBASE CONFIG HERE
+        // PASTE FIREBASE CONFIG HERE
+    };
+
+    async function handleSignout() {
+        await signOut(auth);
+    }
+
+    async function handleSignin() {
+        await signInWithRedirect(auth, new GoogleAuthProvider())
+    }
+
+    function createFirebaseApp() {
+        try {
+            return getApp();
+        } catch {
+            return initializeApp(firebaseConfig);
+        }
+    }
+
+    // Initialize Firebase
+    const app = createFirebaseApp();
+    const auth = getAuth(app);
+    const db = getFirestore(app);
+
+    let user: User|null;
+    
+    onAuthStateChanged(auth, (data) => {
+        user = data;
+    })
+
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Auth bug replication</h1>
+<p>To replicate the bug, open multiple tabs and log in and out several times. After a while, the flow breaks. I could replicate the bug in Chrome and Brave, but not Edge.</p>
 
-<Auth />
-<p>To replicate the bug, open multiple tabs and log in and out several times. After a while, the flow breaks.</p>
+<div>
+    {#if user}
+        <span>{user.displayName}</span>
+        <input type="button" value="Sign out" on:click={handleSignout} />
+    {:else}
+        <input type="button" value="Sign in with Google" on:click={handleSignin} />
+    {/if}
+</div>
